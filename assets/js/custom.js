26582119bbbde4be770d42527707dc78a3746b54
Lakshay355 <lakshaymehra355@gmail.com>
2021-03-18 20:50:45 +0530

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,49 @@
+
+
+
+
+(function ($) {
+    "use strict";
+    var mainApp = {
+
+        main_fun: function () {
+            /*====================================
+               CUSTOM LINKS SCROLLING FUNCTION 
+              ======================================*/
+
+            $('a[href*=#]').click(function () {
+                if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+               && location.hostname == this.hostname) {
+                    var $target = $(this.hash);
+                    $target = $target.length && $target
+                    || $('[name=' + this.hash.slice(1) + ']');
+                    if ($target.length) {
+                        var targetOffset = $target.offset().top;
+                        $('html,body')
+                        .animate({ scrollTop: targetOffset }, 600); //set scroll speed here
+                        return false;
+                    }
+                }
+            });
+
+
+
+
+        },
+
+        initialization: function () {
+            mainApp.main_fun();
+
+        }
+
+    }
+
+
+    $(document).ready(function () {
+        mainApp.main_fun();
+    });
+
+}(jQuery));
+
+
+
